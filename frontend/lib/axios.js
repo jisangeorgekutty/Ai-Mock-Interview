4b3c5e9078465457efe2631548c65b801ed9ff44
Jisan George <64075088+jisangeorgekutty@users.noreply.github.com>
2025-06-12 21:52:49 +0530

    Update axios.js

--- frontend/lib/axios.js
+++ frontend/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance=axios.create({
-    baseURL:"http://localhost:5000/api",
+    baseURL:"https://mock-master-backend.onrender.com",
     withCredentials:true
 });
