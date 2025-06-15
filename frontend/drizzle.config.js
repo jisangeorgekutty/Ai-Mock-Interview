907acb85ea55b93253362cee93fdd5c2df236caa
Jisan George <64075088+jisangeorgekutty@users.noreply.github.com>
2025-06-15 14:52:22 +0530

    done some changes

--- frontend/drizzle.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from "drizzle-kit";
-export default defineConfig({
-  dialect: "postgresql",
-  schema: "./utils/schema.js",
-  dbCredentials:{
-    url:'postgresql://ai_owner:Zpujac4IrMV8@ep-morning-art-a5nud72w.us-east-2.aws.neon.tech/ai-mock-interview?sslmode=require',
-  }
-});
